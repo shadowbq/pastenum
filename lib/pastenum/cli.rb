1f4537383740dbbc526e822f9c00924d75157935
shadowbq <shadowbq@gmail.com>
2013-02-13 21:50:05 -0500

    fixed report output for github targets

--- lib/pastenum/cli.rb
+++ lib/pastenum/cli.rb
@@ -4,37 +4,43 @@ require 'colored'
 
 
 module Pastenum
-  module CLI
+  class CLI
   
-  
-    puts "++++++++++++++++++++++++++++++++++++++++++++++".green
-    puts "+ Pastie Enum".green
-    puts "+ A Corelan Team Production - www.corelan.be".green
-    puts "+ Written by Nullthreat".green
-    puts "+ Version 2".green
-    puts "++++++++++++++++++++++++++++++++++++++++++++++\n".green
-    puts "[?] Input a search string:".green
+    def self.invoke
+      puts "++++++++++++++++++++++++++++++++++++++++++++++".green
+      puts "+ Pastie Enum".green
+      puts "+ A Corelan Team Production - www.corelan.be".green
+      puts "+ Written by Nullthreat".green
+      puts "+ Version 2".green
+      puts "++++++++++++++++++++++++++++++++++++++++++++++\n".green
+      puts "[?] Input a search string:".green
     
-    dork = gets
+      dork = gets
         
-    @gist = Pastenum::Gist.new(dork)
-    @gist.search
-    @gist.summary
+      @gist = Pastenum::Gist.new(dork)
+      @gist.max_pages = 2
+      @gist.search
+      @gist.summary
     
-    @github = Pastenum::Github.new(dork)
-    @github.search
-    @github.summary
+      @github = Pastenum::Github.new(dork)
+      @github.max_pages = 2
+      @github.search
+      @github.summary
     
-    @pastebin = Pastenum::Pastebin.new(dork)
-    @pastebin.search
-    @pastebin.summary
+      @pastebin = Pastenum::Pastebin.new(dork)
+      @pastebin.max_pages = 2
+      @pastebin.search
+      @pastebin.summary
 
-    @pastie = Pastenum::Pastie.new(dork)
-    @pastie.search
-    @pastie.summary
+      @pastie = Pastenum::Pastie.new(dork)
+      #@pastie.max_pages = 2
+      @pastie.search
+      @pastie.summary
 
 
 
-    Pastenum::Report.new(dork, @pastie.results, @pastebin.results, @github.results, @gist.results).to_file
+      Pastenum::Report.new(dork, @pastie.results, @pastebin.results, @github.results, @gist.results).to_file
+    end
+
   end
 end
--- lib/pastenum/paste_report.rb
+++ lib/pastenum/paste_report.rb
@@ -16,25 +16,14 @@ module Pastenum
     
       # Header
     
-      file.write("<html><head><title>Pastesearch Results for #{@dork}</title><link rel=\"stylesheet\" type=\"text/css\" href=\"lib/css/main.css\" media=\"screen\" /></head>")
+      file.write("<html><head><title>Pastesearch Results for #{@dork}</title><link rel=\"stylesheet\" type=\"text/css\" href=\"lib/css/main.css\" media=\"screen\" /></head>\n")
     
-      file.write("<body><div id=\"container\"><div id=\"logo\"><img src=\"lib/img/logo.gif\" width=\"114\" height=\"36\" /></div>")
-      file.write("<div id=\"menu\">Pastesearch: This can take some time to render</div>")
+      file.write("<body><div id=\"container\"><div id=\"logo\"><img src=\"lib/img/logo.gif\" width=\"114\" height=\"36\" /></div>\n")
+      file.write("<div id=\"menu\">Pastesearch: This can take some time to render</div>\n")
       file.write("<div id=\"content\">")
         
-      #Pastie.org
-    
-      if @addresses_pastie.nil? == false
-        file.write("<h2>Search Term: \"#{@dork}\" <br /> Site: Pastie - Found: #{@addresses_pastie.count} Items</h2>\n")
-        @addresses_pastie.each do |links|
-          file.write("<p><iframe src=#{links}/text style=\"width: 100%; height: 300px; background-color: white\"></iframe><p>link: <a href=\"#{links}\" target=\"_blank\">#{links}</a>\n")
-          print ".".green
-        end
-        file.write("<hr><hr>")
-      end
-    
       # Pastebin.com
-      if @addresses_pastebin.nil? == false         
+      unless @addresses_pastebin.empty?         
         file.write("<br /><h2>Site: Pastebin - Found: #{@addresses_pastebin.count} Items</h2>\n")  
         @addresses_pastebin.each do |links|
           file.write("<p><iframe src=\"http://pastebin.com/embed_iframe.php?i=#{links}\" style=\"border:none;width:100%;height:300px\"></iframe><p>link: <a href=\"http://pastebin.com/#{links}\" target=\"_blank\">http://pastebin.com/#{links}</a>\n")
@@ -43,30 +32,40 @@ module Pastenum
         file.write("<hr><hr>")
         end
     
-      #Github.com
-      if @addresses_github.nil? == false
-        file.write("<br /><h2>Site: Github - Found: #{@addresses_github.count} Items</h2>\n")
-        @addresses_github.each do |links|
-          file.write("<p><iframe src=#{links} style=\"width: 100%; height: 300px; background-color: white\"></iframe><p>link: <a href=\"#{links}\" target=\"_blank\">#{links}</a>\n")
-          print ".".green
-        end
-      end
-    
       #gist.github.com
-      if @addresses_gist.nil? == false
+      unless @addresses_gist.empty?
         file.write("<br /><h2>Site: Github Gist - Found: #{@addresses_gist.count} Items</h2>\n")
         @addresses_gist.each do |links|
           file.write("<p><script src=\"https://gist.github.com#{links}.js\"></script><p>link: <a href=\"#{links}\" target=\"_blank\">#{links}</a>\n")
           print ".".green
         end
       end
-    
+      
+      #Pastie.org
+      unless @addresses_pastie.empty?
+        file.write("<h2>Search Term: \"#{@dork}\" <br /> Site: Pastie - Found: #{@addresses_pastie.count} Items</h2>\n")
+        @addresses_pastie.each do |links|
+          file.write("<p><iframe src=#{links}/text style=\"width: 100%; height: 300px; background-color: white\"></iframe><p>link: <a href=\"#{links}\" target=\"_blank\">#{links}</a>\n")
+          print ".".green
+        end
+        file.write("<hr><hr>")
+      end
+      
+      #Github.com
+      # Github passes x-frame-options	deny so you can not iframe this result.
+      unless @addresses_github.empty?
+        file.write("<br /><h2>Site: Github - Found: #{@addresses_github.count} Items</h2>\n")
+        @addresses_github.each do |links|
+          file.write("<p>link: <a href=\"#{links}\" target=\"_blank\">#{links}</a></p>\n")
+          print ".".green
+        end
+      end    
       # Footer
     
-      file.write("</div>")
-      file.write("<div id=\"footer\">&copy; Corelan Team | Written by Nullthreat</div>")
-      file.write("<center><img src=\"lib/img/corelanlogo_small.jpg\"></center>")
-      file.write("</div></body></html>")
+      file.write("</div>\n")
+      file.write("<div id=\"footer\">&copy; Corelan Team | Written by Nullthreat\n")
+      file.write("<center><img src=\"lib/img/corelanlogo_small.jpg\"></center></div>\n")
+      file.write("</div></body></html>\n")
       file.close
     
       puts "\n[*] HTML Report Created".green
