ada561f1a72225b2886d1fe6a861186bd2795bf0
shadowbq <shadowbq@gmail.com>
2013-02-18 04:01:55 +0000

    fixed pastie.org finally, still need to due raw and STDOUT to disable

--- lib/pastenum/targets/pastie.rb
+++ lib/pastenum/targets/pastie.rb
@@ -1,4 +1,8 @@
 #Internal search implementation for pastie.org (deprecated, migrate to Gscraper)
+# Standard link
+# http://pastie.org/pastes/6208073
+# Raw link
+# http://pastie.org/pastes/6208073/text
 module Pastenum
   class Pastie < Target
     
@@ -6,52 +10,33 @@ module Pastenum
       @dork = URI.escape(dork)
       @agent = Mechanize.new
       @results = Array.new
-      @vendor = "pastie.org"
+      @vendor = "http://pastie.org"
       super
     end
 
     def search
+      puts "[*] Searching Pastebin.com (Limit: First #{@max_pages} Pages)".green if @verbose
+      q = GScraper::Search.query(:query => @dork + " inurl:pastes -inurl:page", :site => 'pastie.org')
       print "[*] Parsing pages:".green if @verbose
-      current_page = 1
-      page_numbers.times do
-        print ".".green if @verbose
-        page = @agent.get("http://pastie.org/search?&commit=Start+Search&page=#{current_page}&q=#{@dork}")
-        page.links.each do |link|
-          if link.href.match(/pastie\.org\/pastes/)
-            @results << link.href
+      begin
+        for i in 1..@max_pages do
+          print ".".green if @verbose
+          page = q.page(i)
+          page.each do |result|
+            if result.url.to_s.match(/pastes\/[0-9]+$/)
+              # url -> "http://pastie.org/pastes/6700343"
+              # to_s.match(/pastes\/([0-9]+)$/)[1]
+              code = result.url.to_s.split("/").last
+              # code -> "6700343"
+              @results << code
+            end
           end
         end
-        current_page += 1
+      rescue
+        raise TargetUnreachable, "[!] ERROR: Google search unreachable , Maybe the googles banned you?"
       end
       puts "\n" if @verbose
       return @results.uniq!
-    end
-  
-    private
-  
-    def page_numbers
-      puts "[*] Getting Results".green
-      begin
-        results = @agent.get("http://pastie.org/search?&commit=Start+Search&page=1&q=#{@dork}")
-      rescue
-        raise TargetUnreachable, "[!] ERROR: Can not load pastie.org - Check Connectivity"
-      end
-      puts "[*] Searching Pastie.org (Limit: 1000 Results)".green if @verbose
-      page_count = []
-      results.links.each do |link|
-        if link.href.match(/pastie.org\/pastes\//)
-          page_count << 1
-        else
-          page_count << 0
-        end
-        if link.href.match(/Start%2BSearch&page=/)
-          uri = link.href
-          url = URI("http://pastie.org/search/" + uri)
-          page_num = url.query_params['page']
-          page_count << page_num.to_i
-        end
-      end
-      page_count.max
     end
   
   end
